3e26760e80708157015e41be39a02ea84539b035
Chak Man Andrew Yip <yipc@kl-16160.kaust.edu.sa>
2015-12-06 15:00:06 +0300

    write create_widget.feature, create_widget_steps.js

---
+++ tests/cucumber/features/step_definitions/create_widget_steps.js
@@ -0,0 +1,20 @@
+module.exports = function() {
+  this.Given(/^I am logged in$/, function () {
+    // Write the automation code here
+    pending();
+  });
+
+  this.When(/^I fill in the name with "([^"]*)"$/, function (arg1) {
+    // Write the automation code here
+    pending();
+  });
+  this.When(/^I click the button "([^"]*)"$/, function (arg1) {
+    // Write the automation code here
+    pending();
+  });
+
+  this.Then(/^I should see a widget named "([^"]*)"$/, function (arg1) {
+    // Write the automation code here
+    pending();
+  });
+}
